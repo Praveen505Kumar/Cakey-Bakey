82abbcfd0dfaea0b06224c86d4354994185649b0
praveenkumar505987 <praveenkumar505987@gmail.com>
2022-07-15 08:48:23 +0530

    front sample

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import * as api from "./api/api";
 import './App.css';
 import { useState, useEffect } from "react";
-
-
+//import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./components/Home"
 function App() {
 
   const [categories, setCategories] = useState([]);
@@ -22,6 +22,13 @@ function App() {
 
   return (
     <div className="App">
+      {/* <BrowserRouter>
+        <Routes>
+          <Route exact path="/" component={Home} />
+        </Routes>
+        <Route path="/add-list" component={CreateTodo} />
+      </BrowserRouter> */}
+      <Home />
       <ul>{categories.map((item, index) => (
         <li key={index}>{item.name}</li>
       ))}</ul>
---
+++ client/src/components/Home/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <div>
+            <p>Logo</p>
+            <h3>Cakey Bakey</h3>
+            <button>Sign Up</button>
+            <button>Sign In</button>
+        </div>
+    );
+}
+
+export default Header;
