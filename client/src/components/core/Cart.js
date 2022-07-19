3d03558013008c74e0f56948a8b033cccf5aa369
807995 <kannalimukthi@gmail.com>
2022-07-19 13:11:22 +0530

    updated

--- client/src/components/core/Cart.js
+++ client/src/components/core/Cart.js
@@ -1,6 +1,14 @@
+import img5 from "../images/seller1.webp"
 const Cart = () => {
     return (
-        <div className="sticky-md-top bg-secondary cart-position">Cart</div>
+        // <div className="sticky-md-top bg-1 cart-position"><h1>Cart</h1>
+        // <div className=" text-center">
+        //  <div className=" text-center"><span class='bi bi-cart-fill' style={{ fontSize: 50,height:100}}></span></div>
+        //  <h1>Cart is Empty!</h1>
+        //  </div>
+        // </div>
+
+
     );
 }
 
