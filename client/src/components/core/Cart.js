b2f9e51eec33090f3c32a04bc24d2d4cfd8c6c14
807995 <kannalimukthi@gmail.com>
2022-07-19 13:12:38 +0530

    updated

--- client/src/components/core/Cart.js
+++ client/src/components/core/Cart.js
@@ -1,12 +1,13 @@
 import img5 from "../images/seller1.webp"
 const Cart = () => {
     return (
-        // <div className="sticky-md-top bg-1 cart-position"><h1>Cart</h1>
-        // <div className=" text-center">
-        //  <div className=" text-center"><span class='bi bi-cart-fill' style={{ fontSize: 50,height:100}}></span></div>
-        //  <h1>Cart is Empty!</h1>
-        //  </div>
-        // </div>
+         <div className="sticky-md-top bg-1 cart-position"><h1>Cart</h1>
+        {/* <div className=" text-center">
+         <div className=" text-center"><span class='bi bi-cart-fill' style={{ fontSize: 50,height:100}}></span></div>
+         <h1>Cart is Empty!</h1>
+         </div>
+         */}
+         </div>
 
 
     );
