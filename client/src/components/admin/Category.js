1c832fa50961ca6b1d3cc72c92ba31681e8a9352
tejaswi.parapatla <tejaswi.parapatla@gmail.com>
2022-07-31 21:20:26 +0530

    delete category

--- client/src/components/admin/Category.js
+++ client/src/components/admin/Category.js
@@ -1,9 +1,10 @@
-// import Dashboard from "./Dashboard";
+import Dashboard from "./Dashboard";
 import axios from "axios";
-import { useState, useEffect } from "react";
-import CategoryForm from "./CategoryForm"
-import { NavLink } from 'react-router-dom';
-import Navbar from "../core/Navbar";
+import { useState, useEffect, Fragment } from "react";
+import Categoryform from "./CategoryformAdd"
+//import { NavLink } from 'react-router-dom';
+//import Navbar from "../core/Navbar";
+import CategoryformDelete from "./CategoryformDelete";
 const Category = () => {
     const [categories, setCategories] = useState([]);
 
@@ -21,24 +22,25 @@ const Category = () => {
     );
 
     return (
-        <div>
-            <Navbar />
-            <div className="row header-pad">
-                <div className="col-md-2  bg-dark  pe-3 pt-2">
-                    <NavLink to="/userdetails" className="nav-link link" >
+        <Fragment>
+
+            <div className=" header-pad">
+                <Dashboard />
+                {/* <div className="col-md-2 d-lg-block bg-black  pe-3 pt-2">
+                    <NavLink to="/userdetails" className=" nav-link link" >
                         Users
                     </NavLink>
-                    <NavLink to="/category" className="nav-link link" activeClassName="nav-link-active">
+                    <NavLink to="/category" className="nav-link link" activeclassname="nav-link-active">
                         Category
                     </NavLink>
-                    <NavLink to="/products" className="nav-link link" activeClassName="nav-link-active">
+                    <NavLink to="/products" className="nav-link link" activeclassname="nav-link-active">
                         Products
                     </NavLink>
-                    <NavLink to="/orders" className="nav-link link" activeClassName="nav-link-active">
+                    <NavLink to="/orders" className="nav-link link" activeclassname="nav-link-active">
                         Orders
                     </NavLink>
-                </div>
-                <div className="col-md-9 ps-5">
+                </div> */}
+                <div className="col-md-11 adminscrn">
                     <h1>Category</h1>
                     <div className="d-flex justify-content-end p-2">
                         <button type="button" className="btn btn-success" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
@@ -54,7 +56,7 @@ const Category = () => {
                                     <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div className="modal-body">
-                                    <CategoryForm />
+                                    <Categoryform />
                                 </div>
                             </div>
                         </div>
@@ -69,18 +71,32 @@ const Category = () => {
                                 </tr>
                             </thead>
                             <tbody>
-                                {categories.map((item, index) => (
-                                    <tr className="plain-table-row" key={item._id}>
+                                {categories.map((category, index) => (
+                                    <tr className="plain-table-row" key={category._id}>
                                         <td className="plain-table-cell">{index + 1}</td>
-                                        <td className="plain-table-cell">{item.name}</td>
+                                        <td className="plain-table-cell">{category.name}</td>
 
                                         <td className="plain-table-cell">
                                             <a href="/edit" className="btn btn-secondary mx-2">
                                                 Edit
                                             </a>
-                                            <button className="btn btn-danger">
+                                            {/* delete module */}
+                                            <button className="btn btn-danger" data-bs-toggle="modal" data-bs-target={"#staticBackdrop" + index}>
                                                 <i className="fa fa-trash-o"></i> Delete
                                             </button>
+                                            <div className="modal fade" id={"staticBackdrop" + index} data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                                <div className="modal-dialog modal-dialog-centered">
+                                                    <div className="modal-content">
+                                                        <div className="modal-header">
+                                                            <h5 className="modal-title" id="staticBackdropLabel">Are you sure?</h5>
+                                                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                        </div>
+                                                        <div className="modal-body">
+                                                            <CategoryformDelete categoryid={category._id} categoryname={category.name} />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </td>
                                     </tr>
                                 ))}
@@ -90,7 +106,7 @@ const Category = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </Fragment>
     );
 }
 
