405d7aa1d80116b90dbbc0f85d6e9e35ec5ca89b
807995 <kannalimukthi@gmail.com>
2022-07-31 21:51:11 +0530

    product delete completed

---
+++ client/src/components/admin/ProductFormDelete.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const ProductFormDelete = ({ productid, productname }) => {
+    const [error, setError] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const user = JSON.parse(localStorage.getItem("sample"))
+        axios.delete('http://localhost:8000/api/product/' + productid + '/' + user._id)
+            .then(response => {
+                console.log(response.data);
+                window.location.reload();
+            })
+            .catch(error => {
+                setError(error.response.data.error);
+                console.log(error);
+            })
+
+    };
+    return (
+        <div>
+            <form>
+                <div className="row mb-3">
+                    <p>Delete "{productname}"</p>
+                </div>
+                <div className="modal-footer">
+                    <button type="button" className="btn btn-secondory" data-bs-dismiss="modal">Cancel</button>
+                    <button type="submit" className="btn btn-danger" onClick={handleSubmit}>Delete</button>
+                </div>
+                {error && (<div className="alert alert-danger py-2" role="alert">
+                    Error:{error}
+                </div>)}
+            </form>
+        </div>
+    );
+}
+
+export default ProductFormDelete;
--- client/src/components/admin/Products.js
+++ client/src/components/admin/Products.js
@@ -7,34 +7,34 @@ import axios from "axios";
 
 
 const Products = (props) => {
-  const [products, setProducts] = useState([]);
-  const [categories, setCategories] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [categories, setCategories] = useState([]);
 
-  useEffect(() => {
-      axios.get("http://localhost:8000/api/products")
-          .then(response => {
-              setProducts(response.data);
-              console.log(response.data);
-          })
-          .catch(error => {
-              console.log(error);
-          });
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/products")
+            .then(response => {
+                setProducts(response.data);
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
 
-      axios.get("http://localhost:8000/api/categories")
-          .then(response => {
-              setCategories(response.data);
-              console.log(response.data);
-          })
-          .catch(error => {
-              console.log(error);
-          })
-  }, []);
+        axios.get("http://localhost:8000/api/categories")
+            .then(response => {
+                setCategories(response.data);
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }, []);
     return (
         <div>
             <Dashboard />
             <div class="header-pad adminscrn">
-               <h1>Products</h1>
-               <div class="d-flex justify-content-end p-2">
+                <h1>Products</h1>
+                <div class="d-flex justify-content-end p-2">
                     <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                         <i class="bi bi-plus"></i>
                         Add New
@@ -53,9 +53,9 @@ const Products = (props) => {
                             </div>
                         </div>
                     </div>
-                    </div>
-           
-               {/* <div className="card cake-card me-2 my-3 shadow-sm" style={{ width: 230 }}>
+                </div>
+
+                {/* <div className="card cake-card me-2 my-3 shadow-sm" style={{ width: 230 }}>
                <div className="card-body ">
              <img className="card-img-top" src={img1} alt="no image"></img>
             <h5 className="card-title text-left">cake name</h5>
@@ -69,9 +69,9 @@ const Products = (props) => {
             
             </div>
             </div> */}
-              <div className="row mt-1">
-                    <div className="col-md-8 sample"> 
-                  
+                <div className="row mt-1">
+                    <div className="col-md-8 sample">
+
                         {categories && categories.map(category => {
                             return (
                                 <Fragment>
@@ -80,31 +80,32 @@ const Products = (props) => {
                                         <div className="cat-line flex-grow-1 my-auto ms-2"></div>
                                     </div>
                                     <div className="d-flex flex-wrap">
-                                        {products && products.filter(product => product.category.name === category.name).map(product => {
+                                        {products && products.filter(product => product.category.name === category.name).map((product, index) => {
                                             return (
                                                 <Item name={product.name}
                                                     description={product.description}
                                                     price={product.price}
                                                     id={product._id}
                                                     key={product._id}
+                                                    index={index}
                                                 />
                                             )
                                         })}
                                     </div>
-                                    </Fragment>
+                                </Fragment>
                             );
                         })}
                     </div>
-                  </div>
-                  </div>
-                  </div> 
-                
-                    
+                </div>
+            </div>
+        </div>
 
-        
-    
-        
-    
+
+
+
+
+
+
     );
 }
 
