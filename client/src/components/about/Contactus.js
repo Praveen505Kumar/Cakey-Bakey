08dd5e68a692cb2f49f0db3d918240b45350f492
807995 <kannalimukthi@gmail.com>
2022-08-09 13:37:59 +0530

    contact us

--- client/src/components/about/Contactus.js
+++ client/src/components/about/Contactus.js
@@ -8,6 +8,14 @@ const Contactus = () => {
                 <Navbar />
                 <div className="container header-pad">
                     <h1 className="head text-center bg5">CONTACT US</h1>
+                    <h2><strong>Mailto :</strong></h2>
+                    <h4>cakey3bakey@gmail.com</h4><br></br>
+                    <h2><strong>Address :</strong></h2>
+                    <h4>29-2, cakey street</h4>
+                    <h4>gandhi road</h4>
+                    <h4>chittoor district</h4>
+                    <h4>Pincode: 123456</h4>
+                    <h4>Andhra pradesh</h4>
 
 
                 </div>
